4312169a5478c0cd5fa82235ec54aa894d03fa86
EvivNotrub <barthwerle@yahoo.de>
2023-10-12 17:39:05 +0200

    tests unitaire supp test integration incomplet

--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -1,5 +1,6 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, act, waitFor, render, screen } from "@testing-library/react";
 import Home from "./index";
+import { wait } from "@testing-library/user-event/dist/utils";
 
 describe("When Form is created", () => {
   it("a list of fields card is displayed", async () => {
@@ -29,16 +30,23 @@ describe("When Form is created", () => {
 
 
 describe("When a page is created", () => {
-  it("a list of events is displayed", () => {
-    // to implement
-  })
-  it("a list a people is displayed", () => {
-    // to implement
+  it("a list of events is displayed", async () => {
+    render(<Home />);
+    await screen.findByTestId("eventList-testid");
+  });
+  it("a list a people is displayed", async () => {
+    render(<Home />);
+    await screen.findAllByTestId("peopleCard-testid");
   })
   it("a footer is displayed", () => {
-    // to implement
+    render(<Home />);
+    screen.getByTestId("footer-testid");
   })
-  it("an event card, with the last event, is displayed", () => {
-    // to implement
+  it("an event card, with the last event, is displayed", async () => {
+    render(<Home />);
+    await screen.findByText("Notre derniére prestation");
+    const TIMEOUT = 3000; // mockContactApi response + button title loading time
+    await waitFor(() => screen.getByTestId("card-testid"), { timeout: TIMEOUT });
+
   })
 });
